4393b9939942af25dc54ba40ff43b6f68e80840e
Salty-123 <148965141+Salty-123@users.noreply.github.com>
2023-12-16 12:50:42 +0100

    railway token update

--- tests/init.test.js
+++ tests/init.test.js
@@ -7,15 +7,15 @@ const app = require('../index.js');
 
 const { getProfile } = require('../service/ProfileService.js');
 
-test( 'GET Profile by function', async t => {
+/*test( 'GET Profile by function', async t => {
     const result = await getProfile(0,0);
     console.log(result[0])
     t.is(result[0].city, "city");
 
-});
+}); */
 
 
-/* test.before(async (t) => {
+test.before(async (t) => {
     t.context.server = http.createServer(app);
     t.context.prefixUrl = await listen(t.context.server);
     t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
@@ -28,4 +28,4 @@ test.after.always((t) =>{
 test( 'Get a profile', async (t) => {
     const response = await t.context.got("user/0/profile/0").json();
     console.log(response);
-}); */
+}); 
