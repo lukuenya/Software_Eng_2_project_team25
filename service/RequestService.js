6546edf3e2c8434cec3b6ba77ec6b0f3e7150397
MixalisMourtzakis <147641470+MixalisMourtzakis@users.noreply.github.com>
2023-12-20 18:30:50 +0200

    commit n2

--- service/RequestService.js
+++ service/RequestService.js
@@ -53,10 +53,13 @@ exports.delreq = function(userid,requestid) {
 exports.getNotificationsList = function(userid) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {};
-    if (Object.keys(examples).length > 0) {
+    examples['application/json'] = [{
+      'userid':0},
+      {'userid': 1
+    }];
+      if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
-    } else {
+      } else {
       resolve();
     }
   });
--- test/example..test.js
+++ test/example..test.js
@@ -1,5 +1,66 @@
+const http = require('http');
 const test = require('ava').default;
+const listen = require('test-listen');
+const got = require('got');
+const app = require('../index.js');
 
-test('test to pass', (t)=>{
-    t.pass();
+
+test.before(async (t) =>{
+    t.context.server = http.createServer(app);
+    t.context.prefixUrl = await listen(t.context.server);
+    t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json'});
+  });
+  
+  
+  test.after.always((t) => {
+      t.context.server.close();
+  });
+
+
+//test GetNotificationList function
+const {getNotificationsList} =require('../service/RequestService.js');
+
+
+
+test('Get Notifications', async t => {
+   const result = await getNotificationsList();
+   t.is(result.length, 2);
+   t.is(result[0].userid , 0);
 });
+
+
+test('Get Notifications 2' ,async (t) => {
+   const {body , statusCode}= await t.context.got("userid/0/requests").json();
+   console.log(body);
+   console.log(statusCode);
+   t.is(body.length, 2);
+   t.is(body[0].userid , 0);
+   t.is(statusCode, 200);
+});
+
+
+
+//test AddRequest function
+const {addRequest} =require('../service/RequestService.js');
+
+test('Add Request Test', async t => {
+    const result = await delreq();
+    t.pass();
+ }); 
+
+
+//test DeleteRequest function
+const {delreq} =require('../service/RequestService.js');
+
+test('Delete Request Test', async t => {
+    const result = await delreq();
+    t.pass();
+ }); 
+
+//test UpdateRequest function
+const {updateRequest} =require('../service/RequestService.js');
+
+test('Update Request test', async t => {
+    const result = await updateRequest();
+    t.pass();
+ }); 
