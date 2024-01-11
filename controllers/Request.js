8206b6174f007882b71f484f3fffd0bd68a97c96
kostasi12 <95173560+kostasi12@users.noreply.github.com>
2024-01-12 01:10:46 +0200

    Update Request.js

--- controllers/Request.js
+++ controllers/Request.js
@@ -1,9 +1,10 @@
 'use strict';
-
+// Import required modules
 var utils = require('../utils/writer.js');
 var Request = require('../service/RequestService');
 
 module.exports.addRequest = function addRequest (req, res, next) {
+  // Extract userid and body from request parameters
   var userid = req.swagger.params['userid'].value;
   var body = req.swagger.params['body'].value;
   Request.addRequest(userid,body)
@@ -16,6 +17,7 @@ module.exports.addRequest = function addRequest (req, res, next) {
 };
 
 module.exports.delreq = function delreq (req, res, next) {
+  // Extract userid and requestid from request parameters
   var userid = req.swagger.params['userid'].value;
   var requestid = req.swagger.params['requestid'].value;
   Request.delreq(userid,requestid)
@@ -28,6 +30,7 @@ module.exports.delreq = function delreq (req, res, next) {
 };
 
 module.exports.getNotificationsList = function getNotificationsList (req, res, next) {
+   // Extract userid from request parameters
   var userid = req.swagger.params['userid'].value;
   Request.getNotificationsList(userid)
     .then(function (response) {
@@ -39,6 +42,7 @@ module.exports.getNotificationsList = function getNotificationsList (req, res, n
 };
 
 module.exports.updateRequest = function updateRequest (req, res, next) {
+  // Extract userid and requestid from request parameters
   var userid = req.swagger.params['userid'].value;
   var requestid = req.swagger.params['requestid'].value;
   Request.updateRequest(userid,requestid)
