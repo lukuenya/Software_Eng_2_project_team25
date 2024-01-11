ce0df58039f2812d27831f3b00ac69d76568f74f
kostasi12 <95173560+kostasi12@users.noreply.github.com>
2024-01-11 21:10:57 +0200

    Update writer.js

--- utils/writer.js
+++ utils/writer.js
@@ -1,43 +1,38 @@
-var ResponsePayload = function(code, payload) {
+var ResponsePayload = function (code, payload) {
   this.code = code;
   this.payload = payload;
-}
+};
 
-exports.respondWithCode = function(code, payload) {
+exports.respondWithCode = function (code, payload) {
   return new ResponsePayload(code, payload);
-}
+};
 
-var writeJson = exports.writeJson = function(response, arg1, arg2) {
-  var code;
-  var payload;
+var setResponseCode = function (arg1, arg2) {
+  if (arg2 && Number.isInteger(arg2)) {
+    return arg2;
+  } else if (arg1 && Number.isInteger(arg1)) {
+    return arg1;
+  }
+  // Default to 200 if no valid code is provided
+  return 200;
+};
 
-  if(arg1 && arg1 instanceof ResponsePayload) {
-    writeJson(response, arg1.payload, arg1.code);
-    return;
+var setResponseBody = function (arg1, arg2) {
+  if (arg2 && !(arg2 instanceof ResponsePayload)) {
+    return arg2;
+  } else {
+    return arg1;
   }
+};
 
-  if(arg2 && Number.isInteger(arg2)) {
-    code = arg2;
-  }
-  else {
-    if(arg1 && Number.isInteger(arg1)) {
-      code = arg1;
-    }
-  }
-  if(code && arg1) {
-    payload = arg1;
-  }
-  else if(arg1) {
-    payload = arg1;
-  }
+var writeJson = exports.writeJson = function (response, arg1, arg2) {
+  var code = setResponseCode(arg1, arg2);
+  var payload = setResponseBody(arg1, arg2);
 
-  if(!code) {
-    // if no response code given, we default to 200
-    code = 200;
-  }
-  if(typeof payload === 'object') {
+  if (typeof payload === 'object') {
     payload = JSON.stringify(payload, null, 2);
   }
-  response.writeHead(code, {'Content-Type': 'application/json'});
+
+  response.writeHead(code, { 'Content-Type': 'application/json' });
   response.end(payload);
-}
+};
