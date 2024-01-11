580c82e32e65347cd4c500fd053859af3c01cbeb
lukuenya <matthieundumbi@gmail.com>
2024-01-12 01:28:26 +0200

    MacCabe Cycl. Complexity

--- utils/writer.js
+++ utils/writer.js
@@ -1,45 +1,42 @@
-// Constructor for ResponsePayload object.
-var ResponsePayload = function(code, payload) {
-  this.code = code; // HTTP status code
-  this.payload = payload; // payload (e.g. error message)
-}
+// // Constructor for creating a response payload with a code and payload
+var ResponsePayload = function (code, payload) {
+  this.code = code; // HTTP response code
+  this.payload = payload; // The content/data of the response
+};
 
-// Export functions for writing responses with status codes and payloads
-exports.respondWithCode = function(code, payload) {
+// Function to respond with a specific code and payload
+exports.respondWithCode = function (code, payload) {
   return new ResponsePayload(code, payload);
+};
+
+// Separate function to determine response code
+function determineResponseCode(arg1, arg2) {
+  if (arg2 && Number.isInteger(arg2)) {
+    return arg2;
+  } else if (arg1 && Number.isInteger(arg1)) {
+    return arg1;
+  }
+  return 200; // Default code
 }
 
-var writeJson = exports.writeJson = function(response, arg1, arg2) {
-  var code;
-  var payload;
+// Separate function to process payload
+function processPayload(payload) {
+  if (typeof payload === "object") {
+    return JSON.stringify(payload, null, 2);
+  }
+  return payload;
+}
 
-  if(arg1 && arg1 instanceof ResponsePayload) { // If we have a ResponsePayload object
-    writeJson(response, arg1.payload, arg1.code); // Write the response
+// Simplified function to write a JSON response
+var writeJson = (exports.writeJson = function (response, arg1, arg2) {
+  if (arg1 && arg1 instanceof ResponsePayload) {
+    writeJson(response, arg1.payload, arg1.code);
     return;
   }
 
-  if(arg2 && Number.isInteger(arg2)) { // If we have a code
-    code = arg2; // Set the code to the arg2
-  }
-  else {
-    if(arg1 && Number.isInteger(arg1)) {
-      code = arg1;
-    }
-  }
-  if(code && arg1) { // If we have a code and a payload
-    payload = arg1; // Set the payload to the arg1
-  }
-  else if(arg1) { // If we only have a payload
-    payload = arg1; // Set the payload to the arg1
-  }
+  var code = determineResponseCode(arg1, arg2);
+  var payload = processPayload(arg1 || arg2);
 
-  if(!code) { 
-    // if no response code given, we default to 200
-    code = 200;
-  }
-  if(typeof payload === 'object') { // If the payload is an object
-    payload = JSON.stringify(payload, null, 2); // Stringify the payload
-  }
-  response.writeHead(code, {'Content-Type': 'application/json'}); // Write the response
-  response.end(payload); // End the response
-}
+  response.writeHead(code, { "Content-Type": "application/json" });
+  response.end(payload);
+});
