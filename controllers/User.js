4787eef38ec891deb6ed818f1403283082bd77bf
lukuenya <matthieundumbi@gmail.com>
2024-01-12 00:48:41 +0200

    add comments

--- controllers/User.js
+++ controllers/User.js
@@ -7,11 +7,11 @@ var User = require('../service/UserService');
 // Function to add a user
 module.exports.addUser = function addUser (req, res, next) {
   var body = req.swagger.params['body'].value;
-  User.addUser(body)
+  User.addUser(body) // Call the service function
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response); // Write the response
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response); // Write the error response
     });
 };
