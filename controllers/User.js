db58d232b738bb1582470e93aace4b3893953432
kostasi12 <95173560+kostasi12@users.noreply.github.com>
2024-01-12 01:15:22 +0200

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -1,9 +1,11 @@
 'use strict';
 
+// Import required modules
 var utils = require('../utils/writer.js');
 var User = require('../service/UserService');
 
 module.exports.addUser = function addUser (req, res, next) {
+   // Extract user information from the request body
   var body = req.swagger.params['body'].value;
   User.addUser(body)
     .then(function (response) {
